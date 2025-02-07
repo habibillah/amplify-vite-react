946f97e7162b68af5a9214ba8a5f641db4c392b4
Habibi Busri <habibillah@gmail.com>
2025-02-07 17:04:29 +0700

    add login

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -1,11 +1,9 @@
-import { defineAuth } from '@aws-amplify/backend';
+import { referenceAuth } from '@aws-amplify/backend';
 
-/**
- * Define and configure your auth resource
- * @see https://docs.amplify.aws/gen2/build-a-backend/auth
- */
-export const auth = defineAuth({
-  loginWith: {
-    email: true,
-  },
+export const auth = referenceAuth({
+  userPoolId: 'us-east-1_mJh2FK3cj',
+  identityPoolId: 'us-east-1:6f8e58e6-d0c1-4a42-bd24-758d3b93251e',
+  authRoleArn: 'arn:aws:iam::973897519106:role/amplify-du728qifzdkyb-mai-amplifyAuthauthenticatedU-LdfSv6Uz4iHR',
+  unauthRoleArn: 'arn:aws:iam::973897519106:role/amplify-du728qifzdkyb-mai-amplifyAuthunauthenticate-LdfSv6Uz4iHR',
+  userPoolClientId: '2shtlj443b1ac5o92g04c7qiai',
 });
