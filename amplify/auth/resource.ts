4421e037ce63b2f87ac9a20ccdecfdc331d80e9a
Habibi Busri <habibillah@gmail.com>
2025-02-07 18:18:12 +0700

    change

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -3,8 +3,8 @@ import { referenceAuth } from '@aws-amplify/backend';
 export const auth = referenceAuth({
   userPoolId: 'us-east-1_mJh2FK3cj',
   identityPoolId: 'us-east-1:6f8e58e6-d0c1-4a42-bd24-758d3b93251e',
-  authRoleArn: 'arn:aws:iam::973897519106:role/amplify-du728qifzdkyb-mai-amplifyAuthauthenticatedU-LdfSv6Uz4iHR',
-  unauthRoleArn: 'arn:aws:iam::973897519106:role/amplify-du728qifzdkyb-mai-amplifyAuthunauthenticate-bHz8eWkhbMeK',
+  authRoleArn: 'arn:aws:iam::973897519106:role/service-role/amplify-du728qifzdkyb-mai-amplifyAuthauthenticatedU-LdfSv6Uz4iHR',
+  unauthRoleArn: 'arn:aws:iam::973897519106:role/service-role/amplify-du728qifzdkyb-mai-amplifyAuthunauthenticate-bHz8eWkhbMeK',
   userPoolClientId: '2shtlj443b1ac5o92g04c7qiai',
 
   // userPoolId: 'us-east-1_XUjwxCiBE',
